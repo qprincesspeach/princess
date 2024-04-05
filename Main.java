b41d35deef4590a373d7d80adca379608e05a026
qprincesspeach <165810521+qprincesspeach@users.noreply.github.com>
2024-04-05 07:19:33 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+abstract class Mario{
+  public abstract void attack();
+  public abstract void defend();
+}
+class SuperMario extends Mario{
+  public void attack(){
+    System.out.println("Super Mario attacks with green fire ball!");
+  }
+  public void defend(){
+    System.out.println("SuperMario defends with red shell!");
+  }
+ }
+class Main{
+  public static void main(String[] args){
+    SuperMario yoshi = new SuperMario();
+    yoshi.attack();
+    yoshi.defend();
+   }
+  }
